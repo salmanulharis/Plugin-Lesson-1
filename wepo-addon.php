8cbaf8d5ced3fa5876060103dc3ad6904e1f0c24
salmanulharis <76678624+salmanulharis@users.noreply.github.com>
2024-01-15 16:37:55 +0530

    initial commit

---
+++ wepo-addon.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @package WEPOAddon
+*/
+/*
+Plugin Name: WEPO Addon
+Plugin URI: https://themehigh.com/plugin
+Description: This is a addon plugin for the WEPO Plugin
+Version: 1.0.0
+Author: Themehigh
+Author URI: https://themehigh.com
+License: GPLv2 or later
+Text Domain: wepo-addon
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die;
+}
+
+class WepoAddon {
+
+	function __construct(){
+		add_action('init', array($this, 'custom_post_type'));
+	}
+
+	function activate(){
+		//generated a Custom Post Type
+		$this->custom_post_type();
+		//flush rewrite rules
+		flush_rewrite_rules();
+
+	}
+
+	function deactivate(){
+		//flush rewrite rules
+		flush_rewrite_rules();
+
+	}
+
+	function uninstall(){
+		//delete CPT
+		//delete all the plugin data from the db
+
+	}
+
+	function custom_post_type(){
+		register_post_type('book', ['public' => true, 'label' => 'Books']);
+	}
+
+}
+
+if(class_exists('WepoAddon')){
+	$wepoAddon = new WepoAddon();
+}
+
+//activation
+register_activation_hook(__FILE__, array($wepoAddon, 'activate'));
+
+//deactivation
+register_deactivation_hook(__FILE__, array($wepoAddon, 'deactivate'));
