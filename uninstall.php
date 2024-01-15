de16ac36d2c0eafe76f22a6f0f798f9e565444fb
salmanulharis <76678624+salmanulharis@users.noreply.github.com>
2024-01-15 17:04:22 +0530

    Unistal file - clear db using SQL queries instead of functions

--- uninstall.php
+++ uninstall.php
@@ -12,8 +12,18 @@ if( ! defined( 'WP_UNINSTALL_PLUGIN' ) ){
 
 // Clear Database stored data
 
-$books = get_posts( array( 'post_type' => 'book', 'numberposts' = -1 ));  // numberpost = -1 used to get all the posts
+// $books = get_posts( array( 'post_type' => 'book', 'numberposts' = -1 ));  // numberpost = -1 used to get all the posts
 
-foreach ($books as $book) {
-	wp_delete_post($book->ID, true);
-}
+// foreach ($books as $book) {
+// 	wp_delete_post($book->ID, true);
+// }
+
+////////
+// To delete everything that plugin have
+////////
+
+// Access the DB via SQL
+global $wpdb;
+$wpdb->query( "DELETE FROM wp_posts WHERE post_type = 'book'" )
+$wpdb->query( "DELETE FROM wp_postsmeta WHERE post_id NOT IN (SELECT id FROM wp_posts)" )
+$wpdb->query( "DELETE FROM wp_term_relationships WHERE object_id NOT IN (SELECT id FROM wp_posts)" )
