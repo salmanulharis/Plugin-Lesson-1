9938659175398ef7f617d7c1bfb4bb092f355906
salmanulharis <76678624+salmanulharis@users.noreply.github.com>
2024-01-25 11:09:04 +0530

    settings link below the plugin listing to access the custome administration area

--- wepo-addon.php
+++ wepo-addon.php
@@ -21,13 +21,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 if( !class_exists('WepoAddon') ){
 	class WepoAddon {
 
+		public $plugin;
+
 		function __construct(){
 			add_action('init', array($this, 'custom_post_type'));
+			$this->plugin = plugin_basename(__FILE__);
 		}
 
 		function register(){
 			add_action('admin_enqueue_scripts', array($this, 'enqueue'));
 			add_action('admin_menu', array($this, 'add_admin_pages'));
+			add_filter("plugin_action_links_$this->plugin", array($this, 'settings_link'));
+		}
+
+		public function settings_link($links){
+			$settings_link = '<a href="admin.php?page=wepoaddon_plugin">Settings</a>';
+			array_push($links, $settings_link);
+			return $links;
 		}
 
 		public function add_admin_pages(){
